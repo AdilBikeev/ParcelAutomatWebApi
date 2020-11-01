0a9ea36ee5569a1e326c0cf217162992b7bd290c
AdCoder <bikeev1999@mail.ru>
2020-11-01 12:42:22 +0300

    feature/Postamat Реализовал метод получения инф. по постамату

---
+++ WebApi/Data/ParcelAutomatRepo/IParcelAutomatRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Data.ParcelAutomatRepo
+{
+    public interface IParcelAutomatRepo
+    {
+        /// <summary>
+        /// Возвращает инфрмацию о постамате по его идентификатору.
+        /// </summary>
+        /// <param name="postamatId">Идентификатор постамата.</param>
+        public ParcelAutomat GetParcelAutomat(int postamatId);
+    }
+}
---
+++ WebApi/Data/ParcelAutomatRepo/MockParcelAutomatRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Data.ParcelAutomatRepo
+{
+    public class MockParcelAutomatRepo : IParcelAutomatRepo
+    {
+        /// <summary>
+        /// Список постаматов.
+        /// </summary>
+        private readonly IList<ParcelAutomat> _parcelAutomats = ImmutableList.Create<ParcelAutomat>
+        (
+            new ParcelAutomat()
+            {
+                NumberPostDeliver = 0,
+                Address = "г. Москва, ул. Пр. Вернадского 78"
+            }    
+        );
+
+        public ParcelAutomat GetParcelAutomat(int postamatId) => this._parcelAutomats.FirstOrDefault(item => item.NumberPostDeliver == postamatId);
+    }
+}
