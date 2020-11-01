23a2e3ab811e92357c929befdbe44104872e7f79
AdCoder <bikeev1999@mail.ru>
2020-11-01 12:12:29 +0300

    feature/Postamat Добавил сущность "Постамат" и связал с Order

---
+++ WebApi/Models/ParcelAutomat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    /// <summary>
+    /// Постамат.
+    /// </summary>
+    public class ParcelAutomat
+    {
+        /// <summary>
+        /// Флаг по умолчанию, указывающий на рабочее состояние постамата.
+        /// </summary>
+        private const bool _isOpenDefault = true;
+
+        /// <summary>
+        /// Номер поставмата.
+        /// </summary>
+        [Key]
+        public int NumberPostDeliver { get; set; }
+
+        /// <summary>
+        /// Адрес поставмата.
+        /// </summary>
+        [Required]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Статус поставмата.
+        /// </summary>
+        [Required]
+        [DefaultValue(_isOpenDefault)]
+        public bool IsOpen { get; set; }
+    }
+}
